536ffaf42b7d6f32adfe73fc2ce22d192e372750
polandll <lorinapoland@gmail.com>
2020-11-09 21:25:10 -0800

    simplify curl cmds

--- modules/developers-guide/examples/bash/rest_curl_ks_dcs.sh
+++ modules/developers-guide/examples/bash/rest_curl_ks_dcs.sh
@@ -1,7 +1,7 @@
-curl --location --request POST 'localhost:8082/v2/schemas/keyspaces' \
---header "X-Cassandra-Token: $AUTH_TOKEN" \
---header 'Content-Type: application/json' \
---data '{
-    "name": "users_keyspace-dcs",
+curl -L -X POST 'localhost:8082/v2/schemas/keyspaces' \
+-H "X-Cassandra-Token: $AUTH_TOKEN" \
+-H 'Content-Type: application/json' \
+-d '{
+    "name": "users_keyspace_dcs",
     "datacenters": [ {"name": "dc1"}, {"name": "dc2", "replicas": 5} ]
 }'
