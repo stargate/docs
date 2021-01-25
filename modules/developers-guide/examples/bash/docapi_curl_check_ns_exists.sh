68c3c272f7442d58004d26851a51a9ab287ce041
Théo LEBRUN <mib.ftw@gmail.com>
2021-01-25 16:25:21 -0500

    Replace quotes by double quotes to avoid 401 (#65)

--- modules/developers-guide/examples/bash/docapi_curl_check_ns_exists.sh
+++ modules/developers-guide/examples/bash/docapi_curl_check_ns_exists.sh
@@ -1,3 +1,3 @@
 curl -L -X GET 'localhost:8082/v2/schemas/namespaces' \
--H 'X-Cassandra-Token: $AUTH_TOKEN' \
+-H "X-Cassandra-Token: $AUTH_TOKEN" \
 -H 'Content-Type: application/json'
--- modules/developers-guide/examples/bash/rest_curl_change_column.sh
+++ modules/developers-guide/examples/bash/rest_curl_change_column.sh
@@ -1,6 +1,6 @@
 curl -L \
 -X PUT "http://localhost:8082/v2/schemas/keyspaces/users_keyspace/tables/users/columns/firstname" \
--H 'X-Cassandra-Token: $AUTH_TOKEN' \
+-H "X-Cassandra-Token: $AUTH_TOKEN" \
 -H 'Content-Type: application/json' \ 
  -d '{  
  "name": "first",  
