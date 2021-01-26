7492ec635774561540025c1c88d9f1648f9c0c97
polandll <lorinapoland@gmail.com>
2021-01-25 16:05:24 -0800

    change examples for collections and tuple to use --data-urlencode

--- modules/developers-guide/examples/rest/curl_get_set_data.sh
+++ modules/developers-guide/examples/rest/curl_get_set_data.sh
@@ -3,10 +3,7 @@
 #  "lastname": {"$eq": "Doesha" },
 #  "favorite_books": {"$contains": "Emma"}}
 
-curl -X GET "http://localhost:8082/v2/keyspaces/users_keyspace/users?\
-where=%7B%20%22\
-firstname%22%3A%20%7B%22%24eq%22%3A%20%22Janesha%22%20%7D%2C%20%22\
-lastname%22%3A%20%7B%22%24eq%22%3A%20%22Doesha%22%20%7D%2C%20%22\
-favorite_books%22%3A%20%7B%22%24contains%22%3A%20%22Emma%22%7D%7D" \
- -H  "accept: application/json" \
- -H  "X-Cassandra-Token: 48bcfab2-b1c6-44fd-a56f-9f2221930096"
+curl -G -L "http://localhost:8082/v2/keyspaces/users_keyspace/users" \
+   -H "X-Cassandra-Token: {AUTH_TOKEN}" \
+   -H "Content-Type: application/json" \
+   --data-urlencode 'where={"firstname": {"$eq": "Janesha"}, "lastname": {"$eq": "Doesha"}, "favorite_books": {"$contains": "Emma"}}'
--- modules/developers-guide/examples/rest/curl_get_tuple_data.sh
+++ modules/developers-guide/examples/rest/curl_get_tuple_data.sh
@@ -3,10 +3,7 @@
 #   "lastname": {"$eq": "Doesha" },
 #   "current_country": {"$eq": "( 'France', '2016-01-01', '2020-02-02' )"}}
 
-curl -X GET "http://localhost:8082/v2/keyspaces/users_keyspace/users?\
-where=%20%20%7B%20%22\
-firstname%22%3A%20%7B%22%24eq%22%3A%20%22Janesha%22%20%7D%2C%20%20%20%20%22\
-lastname%22%3A%20%7B%22%24eq%22%3A%20%22Doesha%22%20%7D%2C%20%20%20%20%22\
-current_country%22%3A%20%7B%22%24eq%22%3A%20%22(%20'France'%2C%20'2016-01-01'%2C%20'2020-02-02'%20)%22%7D%7D" \
--H  "accept: application/json" \
--H  "X-Cassandra-Token: d8cfe62a-5648-4067-9758-f426da895da4"
+curl -G -L "http://localhost:8082/v2/keyspaces/users_keyspace/users" \
+   -H  "X-Cassandra-Token: {AUTH_TOKEN}" \
+   -H  "Content-Type: application/json" \
+   --data-urlencode 'where={"firstname": {"$eq": "Janesha"}, "lastname": {"$eq": "Doesha"}, "current_country": {"$eq": "( 'France', '2016-01-01', '2020-02-02' )"}}'
