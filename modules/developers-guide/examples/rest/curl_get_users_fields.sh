528140a0cd6307f42256acbccf759d3466639c15
Doug Wettlaufer <doug.wettlaufer@datastax.com>
2021-10-25 16:32:51 -0500

    Update fields explanations based on feedback

--- modules/developers-guide/examples/rest/curl_get_users_fields.sh
+++ modules/developers-guide/examples/rest/curl_get_users_fields.sh
@@ -1,3 +1,3 @@
-curl -s -L -X GET 'http://localhost:8082/v2/keyspaces/users_keyspace/users/Janesha/Doesha?fields=firstname,lastname,top_three_tv_shows \
+curl -s -L -X GET '{base_url}{base_rest_schema}/{rkeyspace}/{rtable}/Janesha/Doesha?fields=firstname,lastname,top_three_tv_shows \
 --header "X-Cassandra-Token: $AUTH_TOKEN" \
 --header "Content-Type: application/json"
