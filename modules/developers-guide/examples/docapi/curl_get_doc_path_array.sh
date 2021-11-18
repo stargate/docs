9f3108ced1def5fdd7465cfe0a5138b78a989b6e
Lorina Poland <lorinapoland@gmail.com>
2021-11-18 10:09:33 -0800

    remove random comments

--- modules/developers-guide/examples/docapi/curl_get_doc_path_array.sh
+++ modules/developers-guide/examples/docapi/curl_get_doc_path_array.sh
@@ -18,14 +18,3 @@
     }
   }
 }
-
-
-
-
-
-12:29
-Lorina Poland (she/her) If that would work.
-12:29
-Eric Borczuk ah ok
-12:30
-well for the above, both documents would match {"friends.[0].firstname": { "$eq": "Mary"}}
