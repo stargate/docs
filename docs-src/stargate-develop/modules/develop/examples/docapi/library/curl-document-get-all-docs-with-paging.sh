9641179325209242c5b92271e6484968caacc88e
Lorina Poland <lorinapoland@gmail.com>
2022-08-11 12:25:32 -0700

    more fixes

--- docs-src/stargate-develop/modules/develop/examples/docapi/library/curl-document-get-all-docs-with-paging.sh
+++ docs-src/stargate-develop/modules/develop/examples/docapi/library/curl-document-get-all-docs-with-paging.sh
@@ -1,2 +1,2 @@
-curl --location --request GET '{base_doc_url}{bse_doc_api}/{namespace}/collections/{collection}?page-state=JGQwODFlYmIyLTQ4OWUtNDI1ZS04NTI1LWEyNTU4NGY0N2JjZADwf_____B_____' \
+curl --location --request GET '{base_doc_url}{base_doc_api}/{namespace}/collections/{collection}?page-state=JGQwODFlYmIyLTQ4OWUtNDI1ZS04NTI1LWEyNTU4NGY0N2JjZADwf_____B_____' \
 --header "X-Cassandra-Token: {auth_token}"
