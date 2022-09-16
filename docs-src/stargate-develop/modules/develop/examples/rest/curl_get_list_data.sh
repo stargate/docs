7c5d9918649c9d9ad1f6f6cb53998d61beacfc51
Lorina Poland <lorinapoland@gmail.com>
2022-09-16 12:26:37 -0700

    fix merge conflict artifacts

--- docs-src/stargate-develop/modules/develop/examples/rest/curl_get_list_data.sh
+++ docs-src/stargate-develop/modules/develop/examples/rest/curl_get_list_data.sh
@@ -1,10 +1,5 @@
-<<<<<<< HEAD:docs-src/stargate-develop/modules/develop/examples/rest/curl_get_list_data.sh
 curl -s -L -G {base_rest_url}{base_rest_schema}/{rkeyspace}/{rtable} \
    -H  "X-Cassandra-Token: {auth_token}" \
-=======
-curl -s -L -G https://localhost:8082/v2/keyspaces/users_keyspace/users \
-   -H  "X-Cassandra-Token: $AUTH_TOKEN" \
->>>>>>> main:modules/developers-guide/examples/rest/curl_get_list_data.sh
    -H  "Content-Type: application/json" \
    -H "Accept: application/json" \
    --data-urlencode 'where={
--- docs-src/stargate-develop/modules/develop/examples/rest/curl_insert_list_data.sh
+++ docs-src/stargate-develop/modules/develop/examples/rest/curl_insert_list_data.sh
@@ -1,10 +1,5 @@
-<<<<<<< HEAD:docs-src/stargate-develop/modules/develop/examples/rest/curl_insert_list_data.sh
 curl -s -X POST {base_rest_url}{base_rest_api}/{rkeyspace}/{rtable} \
 -H  "X-Cassandra-Token: {auth_token}" \
-=======
-curl -s -X POST https://localhost:8082/v2/keyspaces/users_keyspace/users \
--H  "X-Cassandra-Token: $AUTH_TOKEN" \
->>>>>>> main:modules/developers-guide/examples/rest/curl_insert_list_data.sh
 -H  "Content-Type: application/json" \
 -H  "Accept: application/json" \
 -d '{"firstname": "Janesha",
