66d63763e14b8ccbb5bfa5fca70346de3586ebbd
Nikita Eshkeev <neshkeev@yandex.ru>
2022-11-23 01:19:04 +0300

    Fix typo

--- docs-src/stargate-develop/modules/develop/examples/rest/curl_get_users.sh
+++ docs-src/stargate-develop/modules/develop/examples/rest/curl_get_users.sh
@@ -3,5 +3,5 @@ curl -s -L -G '{base_rest_url}{base_rest_api}/{rkeyspace}/{rtable}' \
    -H "Content-Type: application/json" \
    -H "Accept: application/json" \
    --data-urlencode 'where={
-     "firstname": {"in": ["Janesha","Mookie"]}
+     "firstname": {"$in": ["Janesha","Mookie"]}
    }'
