136b98035724600714bc9945f31c5288b8be72b7
Lorina Poland <lorinapoland@gmail.com>
2023-06-26 11:51:56 -0700

    move redoc-cli to GHA

---
+++ playbooks/lib/inject-collector-config.js
@@ -0,0 +1,24 @@
+'use strict'
+
+module.exports.register = function () {
+  this.once('contentAggregated', ({ contentAggregate }) => {
+    for (const { origins } of contentAggregate) {
+      for (const origin of origins) {
+        const ext = origin.descriptor.ext || {}
+        if (!('collector' in ext && ext.collector == null)) continue
+        origin.descriptor.ext = Object.assign(ext, {
+          collector: {
+            run: {
+              command: `gradlew npmRun -Pcommand=collector`,
+              local: true,
+            },
+            scan: {
+              dir: 'build/stargate/api',
+              base: 'modules/develop/attachments',
+            },
+          },
+        })
+      }
+    }
+  })
+}
